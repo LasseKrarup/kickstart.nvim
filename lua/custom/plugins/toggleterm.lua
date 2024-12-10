61c8d635202c5ef1eaaff45b8b4c8c654c991080
Lasse Krarup <latze95@gmail.com>
2024-12-10 21:01:05 +0100

    Add toggleterm with Lazygit on <leader>g

---
+++ lua/custom/plugins/toggleterm.lua
@@ -0,0 +1,29 @@
+local powershell_options = {
+  shell = vim.fn.executable 'pwsh' == 1 and 'pwsh' or 'powershell',
+  shellcmdflag = '-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;',
+  shellredir = '-RedirectStandardOutput %s -NoNewWindow -Wait',
+  shellpipe = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode',
+  shellquote = '',
+  shellxquote = '',
+}
+
+for option, value in pairs(powershell_options) do
+  vim.opt[option] = value
+end
+
+return {
+  {
+    'akinsho/toggleterm.nvim',
+    version = '*',
+    config = function()
+      local Terminal = require('toggleterm.terminal').Terminal
+      local lazygit = Terminal:new { cmd = 'lazygit', direction = 'float' }
+
+      function _lazygit_toggle()
+        lazygit:toggle()
+      end
+
+      vim.api.nvim_set_keymap('n', '<leader>g', '<cmd>lua _lazygit_toggle()<CR>', { noremap = true, silent = true, desc = 'Open Lazy[G]it' })
+    end,
+  },
+}
